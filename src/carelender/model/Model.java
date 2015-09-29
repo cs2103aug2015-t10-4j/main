cda473c9aec27517bf38f7b2881a1d3a72a71a31
Shalom Quek <imsq117@hotmail.com>
2015-09-29 16:25:42 +0800

    Model.java - sample add code

--- src/carelender/model/Model.java
+++ src/carelender/model/Model.java
@@ -1,5 +1,32 @@
 package carelender.model;
 
-public class Model {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Model {
+	
+	private static File eventFile;
+	public static String filename;
+	
+	public Model() {
+		eventFile = new File(filename);
+	}
+	
+	public static void addEvent(String string) {
+		FileWriter fw;
+		try {
+			fw = new FileWriter(eventFile, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+	        bw.write(string);
+	        bw.newLine();
+	        bw.flush();
+	        bw.close();
+	        System.out.println("added to " + eventFile + ": " + string);
+		} catch (IOException e) {
+			System.out.println("Failed to add event");
+			e.printStackTrace();
+		}
+	}
 }
