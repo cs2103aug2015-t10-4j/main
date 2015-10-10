d3bde70b4f18580d75b7641db1a8a1830129aa14
Jia Xun <chaijiaxun@gmail.com>
2015-10-10 12:13:11 +0800

    Coded skeleton for undo manager

---
+++ src/carelender/model/UndoStep.java
@@ -0,0 +1,28 @@
+package carelender.model;
+
+import carelender.model.data.EventList;
+
+/**
+ * Stores a single step for undo
+ */
+public class UndoStep {
+    private EventList undoData;
+    private UndoType undoType;
+
+    public UndoStep(EventList undoData, UndoType undoType) {
+        this.undoData = undoData;
+        this.undoType = undoType;
+    }
+
+    public EventList getUndoData() {
+        return undoData;
+    }
+
+    public UndoType getUndoType() {
+        return undoType;
+    }
+
+    public enum UndoType {
+        ADD, DELETE, UPDATE
+    }
+}
