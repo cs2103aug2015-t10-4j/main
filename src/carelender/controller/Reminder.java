125c8ea654fc313d3c878a388222c79abd4059a2
Bohan Huang <weepineapple@gmail.com>
2015-10-14 18:08:14 +0800

    initialise timer for reminder

---
+++ src/carelender/controller/Reminder.java
@@ -0,0 +1,12 @@
+package carelender.controller;
+
+import java.util.TimerTask;
+
+public class Reminder extends TimerTask {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+
+}
