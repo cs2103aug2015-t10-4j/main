f25bb64c9dcc7bd0ea2f5fd5015f0d2493d48c5c
Shalom Quek <imsq117@hotmail.com>
2015-10-19 23:35:35 +0800

    Added JUnit4 Test file

---
+++ src/carelender/test/CareLenderTest.java
@@ -0,0 +1,13 @@
+package carelender.test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CareLenderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
