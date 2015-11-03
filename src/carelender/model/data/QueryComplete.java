13d6b97e75a24c1508b3c5a0910a8f4446b7d61a
Shalom Quek <imsq117@hotmail.com>
2015-11-03 13:06:11 +0800

    QueryComplete and Uncomplete

---
+++ src/carelender/model/data/QueryComplete.java
@@ -0,0 +1,89 @@
+package carelender.model.data;
+
+import carelender.controller.Controller;
+import carelender.controller.callbacks.OnConfirmedCallback;
+import carelender.model.Model;
+
+public class QueryComplete extends QueryBase{
+	private EventList events;
+	private boolean forComplete;
+	
+    public QueryComplete(Boolean forComplete) {  	
+        super(QueryType.COMPLETE);
+        this.forComplete = forComplete;
+        events = new EventList();
+    }
+
+	//private Event selectedObject; // Used for confirmation
+
+    public void addEvent ( Event e ) {
+        events.add(e.copy());
+    }
+
+    public void setEventList ( EventList e ) {
+        events = e;
+    }
+    
+    @Override
+	public void controllerExecute() {
+    	if (forComplete) {
+    		System.out.println("COMPLETE");
+    	} else {
+    		System.out.println("UNCOMPLETE");
+    	}
+    	
+		/*Controller.clearMessages();
+		final OnConfirmedCallback deleteConfirmedCallback = new OnConfirmedCallback() {
+			@Override
+			public void onConfirmed(boolean confirmed) {
+				if ( confirmed ) {
+					Model.getInstance().deleteEvent(events);
+					Controller.displayMessage("Deleting " + events.size() + " tasks");
+				} else {
+					Controller.displayMessage("Cancelled delete");
+				}
+				Controller.refreshDisplay();
+			}
+		};
+
+        if ( events != null && events.size() > 0) {
+            Controller.getBlockingStateController()
+                    .startConfirmation("Are you sure you want to delete " + events.size() + " events? [Y/N]", deleteConfirmedCallback);
+        }*/
+
+
+		/*final OnEventSelectedCallback deleteCallback = new OnEventSelectedCallback() {
+			@Override
+			public void onChosen(Event selected) {
+				selectedObject = selected;
+				Controller.getBlockingStateController()
+						.startConfirmation("Are you sure you want to delete \"" + selected.getName() + "\"? [Y/N]", deleteConfirmedCallback);
+			}
+		};*/
+
+		/*if ( searchResults.size() == 0 ) {
+			Controller.displayMessage("There is no task called " + getName());
+		} else if ( searchResults.size() > 1 ) {
+			String message = "There are multiple \""+ getName()+"\" tasks, please choose the one to delete.";
+			Controller.getBlockingStateController().startEventSelection(message, searchResults, deleteCallback);
+		} else {
+			deleteCallback.onChosen(searchResults.get(0));
+		}*/
+
+
+	}
+
+	@Override
+	public EventList searchExecute() {
+		EventList returnList = new EventList();
+		
+		/*if (Model.getInstance().retrieveEvent() != null) {
+			for (Event event : Model.getInstance().retrieveEvent()) {
+				if (Search.isEventNameExact(event, getName())) {
+					returnList.add(event.copy());
+				}
+			}
+		}*/
+		return returnList;
+	}
+}
