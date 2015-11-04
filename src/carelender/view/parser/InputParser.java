ddce519b2b4eecf11fd0995b9807be27e27e4a57
Bohan Huang <weepineapple@gmail.com>
2015-11-04 14:28:12 +0800

    Create the skeleton of set command

--- src/carelender/view/parser/InputParser.java
+++ src/carelender/view/parser/InputParser.java
@@ -145,6 +145,12 @@ public class InputParser {
         newCommand.setDescription("Redoes the last undo");
         commandManager.addCommand(newCommand);
 
+        newCommand = new Command("set", QueryType.SET);
+        newCommand.setDescription("Customize setting");
+        newCommand.setUsage("set \"keyword\" \"newValue\" ");
+        newCommand.addKeywords("setting_type", "username,remindertime,startview", CommandKeyword.DataPosition.AFTER);
+        commandManager.addCommand(newCommand);
+
         newCommand = new Command("exit", QueryType.EXIT);
         newCommand.setDescription("Closes the program");
         commandManager.addCommand(newCommand);
@@ -283,6 +289,9 @@ public class InputParser {
             case DELETE:
                 newQuery = parseDeleteCommand(commandParts);
                 break;
+            case SET:
+                newQuery = parseSetCommand(commandParts);
+                break;
             case COMPLETE:
             	if (commandString.equals("complete")) {
             		newQuery = parseCompleteCommand(commandParts, true);
@@ -290,7 +299,6 @@ public class InputParser {
             		newQuery = parseCompleteCommand(commandParts, false);
             	}
                 break;    
-                
             case UPDATE:
                 newQuery = parseUpdateCommand(commandParts);
                 break;
@@ -333,7 +341,17 @@ public class InputParser {
         return newQuery;
     }
 
-    public QueryBase parseSearchCommand ( DateRange[] dateRanges, CommandPart [] commandParts ) {
+    private QueryBase parseSetCommand(CommandPart [] commandParts) {
+        if ( commandParts.length < 2 ) {
+            return new QueryError(ErrorMessages.setNoParameters());
+        }
+        
+        QuerySet querySet = new QuerySet(commandParts[0].getQueryPart(), commandParts[1].getQueryPart());
+        
+        return querySet;
+	}
+
+	public QueryBase parseSearchCommand ( DateRange[] dateRanges, CommandPart [] commandParts ) {
         QueryList queryList = new QueryList();
 
         boolean pass = false;
