f55f18b974e0125893c0705df4863dd3df2d3de9
Bohan Huang <weepineapple@gmail.com>
2015-11-04 15:14:37 +0800

    make setting username works

--- src/carelender/model/data/QuerySet.java
+++ src/carelender/model/data/QuerySet.java
@@ -1,5 +1,8 @@
 package carelender.model.data;
 
+import carelender.model.AppSettings;
+import carelender.model.AppSettings.SettingName;
+
 public class QuerySet extends QueryBase {
 	
 	String keyword;
@@ -7,13 +10,29 @@ public class QuerySet extends QueryBase {
 	
     public QuerySet(String keyword, String value) {
         super(QueryType.SET);
-        keyword = keyword;
-        value = value;
+    	System.out.println("Inside constructor");
+        this.keyword = keyword;
+        this.value = value;
+        System.out.println("Keyword " + keyword);
+        System.out.println("Value " + value);
     }
     
     @Override
 	public void controllerExecute() {
-		System.out.println("HAHAHAHAHAHAHAHAHAHAHAHAHA");
+    	System.out.println("Inside controllerExecute");
+		switch(keyword){
+			case "username":
+				System.out.println("Inside switch username");
+				AppSettings.getInstance().setStringSetting(SettingName.USERNAME, value);
+				//refresh the screen
+				break;
+			case "remindertime":
+				break;
+			case "startview":
+				break;
+			default:
+				break;
+		}
 	}
 
 	@Override
