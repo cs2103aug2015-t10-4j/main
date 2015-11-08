c537f9b020e3e07883014ba0d898926ceb894c73
Bohan Huang <weepineapple@gmail.com>
2015-11-08 15:33:55 +0800

    Refactoring PopupRenderer

--- src/carelender/view/gui/components/PopupRenderer.java
+++ src/carelender/view/gui/components/PopupRenderer.java
@@ -4,11 +4,18 @@ import carelender.model.strings.AppColours;
 import carelender.view.gui.components.CanvasRenderer;
 import carelender.view.gui.components.TextRenderer;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class PopupRenderer extends CanvasRenderer {
 	String message;
+	private final static double arc = 10.0;
+	private final static double tabHeightRatio = 1.0 / 10.0;
+	private final static double fontSizeRatio = 1.0 / 40.0;
+	private final static double textHeightOffsetRatio = 1.0 / 10.0;
+	private final static double textHeightRatio = 9.0 / 10.0;
+	private final static double textPad = 3.0;
+	private final static double wordWidthRatio = 0.6;
+	private final static double lineSpaceRatio = 0.05;
 	
 	public PopupRenderer(String message) {
 		this.message = message;
@@ -24,14 +31,14 @@ public class PopupRenderer extends CanvasRenderer {
 	@Override
 	public void draw( GraphicsContext gc, double x, double y, double width, double height ) {
 		gc.setFill(AppColours.popupBackground);
-		gc.strokeRoundRect(x, y, width, height, 10.0, 10.0);
+		gc.strokeRoundRect(x, y, width, height, arc, arc);
 		gc.setFill(AppColours.popupHeaderBackground);
-		gc.fillRoundRect(x, y, width, height/10.0, 10.0, 10.0);
+		gc.fillRoundRect(x, y, width, height * tabHeightRatio, arc, arc);
 		
-		double fontSize = width / 40.0; //Temporary
+		double fontSize = width * fontSizeRatio;
 		Font font = Font.loadFont("file:res/monaco.ttf", fontSize);
 		TextRenderer message = new TextRenderer();
-		message.setParams(gc, x, y+height/10.0, width, height*9.0/10.0, 3, 3, font, 0.6, 0.05);
+		message.setParams(gc, x, y + height * textHeightOffsetRatio, width, height * textHeightRatio, textPad, textPad, font, wordWidthRatio, lineSpaceRatio);
 		message.addText(this.message);
 		message.drawText(AppColours.popupBackground, AppColours.popupText);
 	}
